ab5324acc3d0d10aa55d765c72f779cb17bd6b18
prostonau <prostonau@mail.ru>
2023-04-17 00:28:36 +0300

    Remove from list

--- src/index.js
+++ src/index.js
@@ -1,87 +1,36 @@
-
-// assert.equal(dateSample(3), false);
-// assert.equal(dateSample(3.312312), false);
-// assert.equal(dateSample(false), false);
-// assert.equal(dateSample(null), false);
-// assert.equal(dateSample(undefined), false);
-// assert.equal(dateSample([3]), false);
-// assert.equal(dateSample(['3']), false);
-// assert.equal(dateSample({ '3.14': '3dec' }), false);
- 
-
-
-// let b = [ '--discrard-next', true, true, 1 ]
-
-
-const chainMaker = {    
-  getLength() {
-    return this.arr.length      
-  },
-  addLink(value) {
-
-    if (!this.arr)  {this.arr = []; 
-                     //console.log("this arr created");
-                    }
-    else { 
-      //console.log("thisLink | this.arr", this.arr)
-     }
-    //console.log("addLink | value", value)
-    if (value == null) value = 'null'
-    this.arr.push(value);
-    //console.log("addLink | this", this)
-    return this; 
-  },
-  removeLink(position) {
-    //console.log("start remove position", position)
-    //alert('a')
-    //console.log("typeof position", typeof position)
-    if (typeof position != 'number' || position > this.arr.length || position <= 0 ){
-    this.arr = []
-    throw new NotImplementedError('You can\'t remove incorrect link!');
+let list = {
+  value: 3,
+  next: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+		    value: 4,
+		    next: {
+			    value: 4,
+			    next: null
+			  }
+		}
+      }
     }
-
-    let output = []
-    let i = 1
-    this.arr.map(e => {
-       //console.log("output", output)
-        if (i!=position) output.push(e)
-        ++i;
-    })
-    
-    this.arr = output
-    
-    return this     
-  },
-  reverseChain() {
-    if(this.arr) this.arr = this.arr.reverse()
-    return this
-  },
-  finishChain() {
-    console.log('this', this)
-    //alert("s")
-    let output = []
-    output = this.arr
-    this.arr = []
-    console.log("output", '( '+output.join(' )~~( ')+' )')         
-    return '( '+output.join(' )~~( ')+' )'
   }
 };
 
-// console.log(  
-//   chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain()
-// )
+console.log(list)
 
-// console.log(
-//   chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain()
-  
-// )
+let data = []
 
-console.log(
-  //chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({ 0: 'first', 1: 'second', 'length': 2 }).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain()
-  chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0)
-  
-)
-//( DEF )~~( 3.14 )~~( 8.963 )~~( [object Object] )
+ //data = ['[next][next][next][value]', '[next][next][next][next][value]']
+  data = ['[next][next][value]']
+
+ list[data[0]] = 1111
+//list[data[1]] = 2222
+
+// list.next.next.next.value = 788888
+// list.next.next.next.next = null
+console.log(list)
 
 
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,366 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+// function removeKFromList(/* l, k */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
+
+
+// class ListNode {
+//      constructor(x) {
+//        this.value = x;
+//        this.next = null;
+//      }
+//    }
+
+   
+
+function  removeKFromList(l,k) {
+
+
+  console.log("l",l)
+
+  function listLength(list) {
+    let tmp = list;
+    let i = 0;
+  
+    while (tmp) {
+      tmp = tmp.next;
+      ++i;
+    }
+
+    return i
+  
+  }
+
+  function getValues(list) {
+    let out = []
+    let tmp = list;
+    let i = 0;
+  
+    while (tmp) {
+      out.push(tmp.value)
+      tmp = tmp.next;
+      ++i;
+    }
+
+    return out
+  
+  }
+
+  function getBadIndexies(list) {
+    let out = []
+    let tmp = list;
+    let i = 0;
+  
+    while (tmp) {
+      if (tmp.value == k) out.push(i)
+      tmp = tmp.next;
+      ++i;
+    }
+
+    return out
+  
+  }
+
+   function getNexts(list) {
+    let out = []
+    let tmp = list;
+    let i = 0;
+  
+    while (tmp) {
+      out.push(tmp.next)
+      tmp = tmp.next;
+      ++i;
+    }
+
+    return out
+  
+  }
+
+ 
+  function printList(list) {
+
+    console.log("list.value", list.value); // выводим текущий элемент
+  
+    if (list.next) {
+      printList(list.next); // делаем то же самое для остальной части списка
+    }
+  
+  }
+
+
+  function generateArray(list, correctlength){
+    let tmp = list
+    let outArray = []
+    let pair = []
+    let flag2 = 0
+    let flag3 = 1
+
+    if (bads.includes(0))
+    {
+      pair = [tmp.next.value, tmp.next.next];
+      outArray.push([...pair])     
+      flag2 = 3
+
+      pair = []
+    }
+    
+
+ 
+      console.log("outArray after 0 = ",outArray)
+
+      for (let i = 0+flag2; i < (correctlength+flag2); ++i){
+      console.log("i= ",i)     
+
+              console.log("correctlength >>>>>>>>>>>>> ", correctlength)
+            if (bads.includes(correctlength) && i == correctlength-1)
+            {
+              console.log("ENterrrrrrrrrrr")
+              if (i==1) {pair = [tmp.next.value, null]; console.log("1111", tmp.next);}
+              if (i==2) {pair = [tmp.next.next.value, null]; console.log("2222");}
+              if (i==3) {pair = [tmp.next.next.next.value, null];console.log("3333");}
+              if (i==4) {pair = [tmp.next.next.next.next.value, null];console.log("4444");}
+              outArray.push([...pair])     
+              flag3 = 0
+              pair = []
+            }
+      
+      if (flag3){
+      if (bads.includes(i)) {
+        
+        if (i==2) pair = [tmp.next.value, tmp.next.next.next];
+        if (i==3) pair = [tmp.next.next.value, tmp.next.next.next.next];
+        if (i==4) pair = [tmp.next.next.next.value, tmp.next.next.next.next.next];
+        if (i==5) pair = [tmp.next.next.next.next.value, tmp.next.next.next.next.next.next];
+        //if (i==6) pair = [tmp.next.next.next.next.next.value, tmp.next.next.next.next.next.next.next];
+        console.log("current bad i = " + i, "pair = " , pair)
+        outArray.push([...pair])       
+      }
+      else{
+
+        if (i==0) pair = [tmp.value, tmp.next];
+        if (i==1) pair = [tmp.next.value, tmp.next.next];
+        if (i==2) pair = [tmp.next.next.value, tmp.next.next.next];
+        if (i==3) pair = [tmp.next.next.next.value, tmp.next.next.next.next];
+        if (i==4) pair = [tmp.next.next.next.next.value, tmp.next.next.next.next.next];
+        if (i==5) pair = [tmp.next.next.next.next.next.value, tmp.next.next.next.next.next.next];      
+        console.log("current good i = " + i, "pair = " , pair)
+        outArray.push([...pair])     
+      }
+      pair = []
+    }
+    
+  }
+   
+
+
+    return outArray
+
+  }
+
+
+  function arrayToList (l,arr) {
+    let tmp = l
+    for (let i = 0; i < (values.length-bads.length); i++){
+      if (i==0) {tmp.value = arr[i][0]; tmp.next = arr[i][1];}
+      if (i==1) {tmp.next.value = arr[i][0]; tmp.next.next = arr[i][1];}
+      if (i==2) {tmp.next.next.value = arr[i][0]; tmp.next.next.next.next = arr[i][1];}
+      if (i==3) {tmp.next.next.next.value = arr[i][0]; tmp.next.next.next.next.next = arr[i][1];}
+    }
+    return tmp
+  }
+
+
+  // function editList(list) {
+  //   let tmp = list 
+  //   let i = 0;
+
+  //   // while (tmp) {
+      
+  //   //   tmp.value;
+  //   //   tmp = tmp.next;
+  //   //   console.log( )
+  //   // }
+
+  //   for (let i = 0; i < (values.length); i++) {
+  //     if (bads.includes(i)){
+  //       tmp.value = values[i+1] 
+  //       tmp.next = nexts[i+1]       
+  //       console.log("Enter with i = ", i, "Do action!" )
+  //       break
+  //     }
+  //     // else{
+  //     //    console.log("Enter with i = ", i, "Do action!" )
+  //     //   tmp.value = values[i] 
+  //     //   tmp.next = nexts[i]
+  //     //   break
+  //     // }
+  //   }
+
+  //   return tmp
+  // }
+
+  // function editList(list) {
+  //   let tmp = list 
+  //   tmp.next = null
+  //   console.log("tmp = ",tmp)
+  //   console.log("typeof tmp = ",typeof tmp)
+  //   let i = 0;
+
+  //   // while (tmp) {
+      
+  //   //   tmp.value;
+  //   //   tmp = tmp.next;
+  //   //   console.log( )
+  //   // }
+
+  //   for (let i = 0; i < (values.length); i++) {
+
+  //     if (nexts[i] == null) 
+  //     {
+  //       console.log("Exit from loop with i = ",i)
+  //       break; 
+  //     }
+      
+  //     if (values[i] == 3){
+  //       if (i==0) {tmp.value = values[i+1];tmp.next = nexts[i+1];++i;}
+  //       // if (i==1) {tmp.next.value = values[i+1];tmp.next.next = nexts[i+1];++i;}
+  //       // if (i==2) {tmp.next.next.value = values[i+1];tmp.next.next.next = nexts[i+1];++i;}
+  //       // if (i==3) {tmp.next.next.next.value = values[i+1];tmp.next.next.next.next = nexts[i+1];++i;}
+  //       // if (i==4) {tmp.next.next.next.next.value = values[i+1];tmp.next.next.next.next.next = nexts[i+1];++i;}
+  //       // if (i==5) tmp.next.next.next.next.next.value = values[i+1];tmp.next.next.next.next.next.next = nexts[i+1]}
+             
+  //       console.log("Enter with i = ", i, "Do Spechal action!" )
+        
+  //     }
+  //     else{
+  //       if (i==0) {tmp.value = values[i];tmp.next = nexts[i]}
+  //       // if (i==1) {tmp.next.value = values[i];tmp.next.next = nexts[i]}
+  //       // if (i==2) {tmp.next.next.value = values[i];tmp.next.next.next = nexts[i]}
+  //       // if (i==3) {tmp.next.next.next.value = values[i];tmp.next.next.next.next = nexts[i]}
+  //       // if (i==4) {tmp.next.next.next.next.value = values[i];tmp.next.next.next.next.next = nexts[i]}
+  //       // if (i==5) {tmp.next.next.next.next.next.value = values[i];tmp.next.next.next.next.next.next = nexts[i]}
+             
+  //       console.log("Enter with i = ", i, "Do Normal action!" )
+  //     }
+
+  //     console.log('PART 2 ',i,'--------------------------------------------')
+
+  //     if (i> 0 && values[i+1] == 3){
+        
+  //       if (i==1) {tmp.next.value = values[i];tmp.next.next = nexts[i+1];++i;}
+  //       if (i==2) {tmp.next.next.value = values[i];tmp.next.next.next = nexts[i+1];++i;}
+  //       if (i==3) {tmp.next.next.next.value = values[i];tmp.next.next.next.next = nexts[i+1];++i;}
+  //       if (i==4) {tmp.next.next.next.next.value = values[i];tmp.next.next.next.next.next = nexts[i+1];++i;}
+  //       // if (i==5) tmp.next.next.next.next.next.value = values[i+1];tmp.next.next.next.next.next.next = nexts[i+1]}
+             
+  //       console.log("Enter with i = ", i, "Do Spechal action!" )
+        
+  //     }
+  //     else{
+       
+  //       if (i==1) {tmp.next.value = values[i];tmp.next.next = nexts[i]}
+  //       if (i==2) {tmp.next.next.value = values[i];tmp.next.next.next = nexts[i]}
+  //       if (i==3) {tmp.next.next.next.value = values[i];tmp.next.next.next.next = nexts[i]}
+  //       if (i==4) {tmp.next.next.next.next.value = values[i];tmp.next.next.next.next.next = nexts[i]}
+  //       // // if (i==5) {tmp.next.next.next.next.next.value = values[i];tmp.next.next.next.next.next.next = nexts[i]}
+             
+  //       console.log("Enter with i = ", i, "Do Normal action!" )
+  //     }
+
+
+
+
+
+  //     console.log('tmp= ',tmp,'')
+  //     console.log('i= ',i,'--------------------------------------------')
+     
+  //   }
+
+  //   return tmp
+  // }
+
+
+  
+  printList(l)
+
+  let length = listLength(l)
+  console.log("listLength = ",length)
+
+  let values = getValues(l)
+  console.log("Values = ",values)
+
+  let bads = getBadIndexies(l)
+  console.log("Bads = ",bads)
+
+  let nexts = getNexts(l)
+  console.log("Nexts = ",nexts)
+
+  // let out = editList(l)
+  // console.log("Out = ",out)
+
+  function printList(list) {
+    let tmp = list;
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>")
+    while (tmp) {
+      console.log("tmp.value",tmp.value," | tmp.next",tmp.next);
+      console.log("-----------");
+      tmp = tmp.next;
+    }
+    console.log("<<<<<<<<<<<<<<<<<<<<<<<<")
+  
+  }
+
+  out = generateArray(l,values.length-bads.length)
+
+  console.log("out = ", out)
+  console.log("outout[1][1] = ", out[1][1])
+
+  return arrayToList(l,out)
+  
+  // printList(out);
+
+
+  // function newEditList(list) {
+  //   let tmp = list;
+  //   let i = 0;
+  
+  //   while (tmp) {
+  //     console.log("tmp.value = " ,tmp.value)
+  //     console.log("tmp.next = ",  tmp.next)
+  //     if (bads.includes(i-1)){
+  //       tmp = nexts[i+1]
+  //     } else {
+  //       tmp = tmp.next;}
+  //     ++i;
+  //   }
+
+  //   return tmp
+  
+  // }
+
+  // values = getValues(l)
+  // console.log("Values 2 = ",values)
+
+  // bads = getBadIndexies(l)
+  // console.log("Bads 2 = ",bads)
+
+  // nexts = getNexts(l)
+  // console.log("Nexts 2 = ",nexts)
+
+  // out = newEditList(l)
+  // console.log("Out 2 = ",out)
+  
+
+  // console.log('-----------------------------------------')
+  // console.log("l output",out)
+  // printList(out)
+
+  }
+
+
 
 module.exports = {
   removeKFromList
