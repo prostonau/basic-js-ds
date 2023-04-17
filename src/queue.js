2e6069abdd001d33036ea0e878b722be044610d5
prostonau <prostonau@mail.ru>
2023-04-17 15:15:36 +0300

    queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+// class ListNode {
+//   constructor(x) {
+//     this.value = x;
+//     this.next = null;
+//   }
+// }
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // https://www.youtube.com/watch?v=j6w_ZJSYNRU&t=2247s
+
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.next = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    let cur = this.head
+    let out 
+
+    while(cur){
+      console.log(cur.value)
+      const node = cur
+      if (!out) {out = node}      
+      cur = cur.next      
+    }
+
+    return out
+
+  }
+
+  enqueue(data) {
+
+    const node = new ListNode(data)
+    if (this.head) {
+      this.tail.next = node
+      this.tail = node
+    }
+    else
+    {
+      this.head = node
+      this.tail = node
+    }
+    
+    this.length++
+
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    const cur = this.head
+    this.head = this.head.next
+    this.length--;
+
+    return cur.value
+   
   }
 }
 
